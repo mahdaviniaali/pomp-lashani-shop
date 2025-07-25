68cadcfd3e7dd4687865d363610199a07337f071
mahdaviniaali <alimahdavinia125@gmail.com>
2025-07-25 19:24:18 +0330

    foter

--- main/products/urls.py
+++ main/products/urls.py
@@ -6,7 +6,7 @@ app_name = 'products'
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('category/<int:pk>-<persianslug:slug>/', ProductListView.as_view(), name='product_list_by_category'),
-    path('<int:pk>-<persianslug:slug>/', ProductDetailView.as_view(), name='product_detail'),
+    path('d/<int:pk>-<persianslug:slug>/', ProductDetailView.as_view(), name='product_detail'),
     path('product/partials/', ProductPartials.as_view(), name="product_partial"),
     path('category/partials/', ProductPartials.as_view(), name="product_list_by_category_partial"),
 ]
