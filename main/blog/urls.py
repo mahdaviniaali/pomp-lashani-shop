7d4f3af3a152468a4a7d4c25b77a896782fee397
mahdaviniaali <alimahdavinia125@gmail.com>
2025-08-29 01:39:17 +0330

    blog

--- main/blog/urls.py
+++ main/blog/urls.py
@@ -5,8 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path('' ,Blog.as_view() ,name='bloglist'),
-    path('post/<int:post_id>/addcomment/', Coments.as_view(), name='add_comment'),
-    path('post/<int:post_id>/comments/', comment_list, name='comment_list'),
-    path('<int:pk>-<persianslug:slug>/' , BlogDitail.as_view() , name='blogdetail'),
+    path('post/<post_id>/addcomment/', Coments.as_view(), name='add_comment'),
+    path('post/<post_id>/comments/', comment_list, name='comment_list'),
+    path('<pk>-<slug>/' , BlogDitail.as_view() , name='blogdetail'),
     path('<slug>/' , BlogDitail.as_view() , name='blogdetail_old')  # برای حفظ سازگاری با لینک‌های قدیمی
 ]
